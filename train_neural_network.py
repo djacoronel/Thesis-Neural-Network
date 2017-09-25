37ef7f81bc532dd735f8c295a5e4869ce2226e6f
djacoronel <djacoronel@gmail.com>
2017-09-25 19:44:39 +0800

    changed model checkpoint directories back to normal

--- train_neural_network.py
+++ train_neural_network.py
@@ -1,8 +1,8 @@
 from training_functions import TrainingSettings as train
 
-model_name_1 = "casualties_test.ckpt"
-model_name_2 = "damagedhouses_test.ckpt"
-model_name_3 = "damagedproperties_test.ckpt"
+model_name_1 = "model/casualties_test.ckpt"
+model_name_2 = "model/damagedhouses_test.ckpt"
+model_name_3 = "model/damagedproperties_test.ckpt"
 
 print("\n\n***********CASUALTIES**************")
 training = train(model_name_1)
