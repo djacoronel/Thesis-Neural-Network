4b0b8d1eeabee09eca19a77575c1a10ffef6a3d2
djacoronel <djacoronel@gmail.com>
2017-10-31 18:01:15 +0800

    Changes to assessment module.

--- assessment.py
+++ assessment.py
@@ -32,8 +32,16 @@ def print_coefficients(attribute_names, coefficients):
     attribute_names.append("Intercept")
     for i in range(len(attribute_names)):
         print(str(attribute_names[i]) + ": " + str(coefficients[i]))
+    attribute_names.remove("Intercept")
+    print("\n\n")
+
+
+def print_values(attribute_names, coefficients):
+    for i in range(len(attribute_names)):
+        print(str(attribute_names[i]) + ": " + str(coefficients[i]))
 
     print("\n\n")
+
 
 def get_coefficients(dataset_source, predictor_list, dependent):
     data = load_data(dataset_source)
@@ -50,14 +58,31 @@ def get_coefficients(dataset_source, predictor_list, dependent):
     return coefficients
 
 
+def compute(coefficients, inputs):
+    values = []
+
+    for i in range(len(inputs)):
+        sum = 0
+
+        for j in range(len(inputs)):
+            sum += coefficients[j] * inputs[j]
+
+        sum += coefficients[-1]
+        sum -= coefficients[i]
+
+        values.append(sum / -coefficients[i])
+
+    return values
+
+
 x_list = ["YEAR", "INTENSITY", "WIND", "SIGNAL", "POP", "DR", "FLR", "HP", "HMB"]
-get_coefficients("Q-CAS.csv",x_list, "CASUALTIES")
+inputs = [1, 1, 1, 1, 1, 1, 1, 1, 1]
+coef = get_coefficients("Q-CAS.csv", x_list, "CASUALTIES")
+val = compute(coef, inputs)
+print_values(x_list, val)
 
 x_list = ["YEAR", "SIGNAL", "POP", "AI", "DR", "FLR", "HP", "HS", "HMA", "HMB"]
-get_coefficients("Q-DAH.csv",x_list, "DAMAGED HOUSES")
+# get_coefficients("Q-DAH.csv", x_list, "DAMAGED HOUSES")
 
 x_list = ["YEAR", "DURATION", "SIGNAL", "AI", "DR", "SR", "HP", "HS"]
-get_coefficients("Q-DAP.csv",x_list, "DAMAGED PROPERTIES")
-
-
-
+# get_coefficients("Q-DAP.csv", x_list, "DAMAGED PROPERTIES")
